87acd7b2e5d3377f90db7295339f5ddee8fbd34c
Youssef Sadek <youssef.sadek@gmail.com>
2020-10-25 19:00:25 +0100

    catching error and exiting process

--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -1,20 +1,21 @@
-import { Container } from "inversify";
-import { ILogger } from "./services/Logger/interface/ILogger";
-import { Logger } from "./services/Logger/classes/Logger";
-import { AuctionMonitorApp } from "./AuctionMonitorApp";
-import { DependencyIdentifier } from "./DependencyIdentifiers";
-import { IConfig } from "./services/Configuration/interface/IConfig";
-import { Config } from "./services/Configuration/classes/config";
-import { IAuthenticationClient } from "./services/Authentication/interface/IAuthenticationClient";
-import { AuthenticationClient } from "./services/Authentication/classes/AuthenticationClient";
-import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
-import { CarOnSaleClient } from "./services/CarOnSaleClient/classes/CarOnSaleClient";
+import { Container } from 'inversify';
+
+import { ILogger } from './services/Logger/interface/ILogger';
+import { Logger } from './services/Logger/classes/Logger';
+import { AuctionMonitorApp } from './AuctionMonitorApp';
+import { DependencyIdentifier } from './DependencyIdentifiers';
+import { IConfig } from './services/Configuration/interface/IConfig';
+import { Config } from './services/Configuration/classes/config';
+import { IAuthenticationClient } from './services/Authentication/interface/IAuthenticationClient';
+import { AuthenticationClient } from './services/Authentication/classes/AuthenticationClient';
+import { ICarOnSaleClient } from './services/CarOnSaleClient/interface/ICarOnSaleClient';
+import { CarOnSaleClient } from './services/CarOnSaleClient/classes/CarOnSaleClient';
 
 /*
  * Create the DI container.
  */
 const container = new Container({
-    defaultScope: "Singleton",
+    defaultScope: 'Singleton',
 });
 
 /*
@@ -36,6 +37,10 @@ const app = container.resolve(AuctionMonitorApp);
  * Start the application
  */
 (async () => {
-    await app.start();
-    process.exit(0)
+    try {
+        await app.start();
+        process.exit(0)
+    } catch (e) {
+        process.exit(-1)
+    }
 })();
